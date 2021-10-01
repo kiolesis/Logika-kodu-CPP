71d10ff79e2fe5c564dca549159f9bc2cee015f8
Kiolesis <78695820+kiolesis@users.noreply.github.com>
2021-10-01 22:23:38 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+char wy(string wyraz)
+{
+	string odw;
+	for(int i=0;i<=wyraz.size();i++)odw[i]=wyraz[wyraz.size()-i];
+	for(int i=0;i<=wyraz.size();i++)cout<<odw[i];
+}
+
+main()
+{
+	cout<<"Podaj wyraz ";
+	string wyraz;
+	cin>>wyraz;
+	wy(wyraz);
+}
