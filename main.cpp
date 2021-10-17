80ed013f5c1b48af02c15bc09ac5b59a36ce1913
Kiolesis <78695820+kiolesis@users.noreply.github.com>
2021-10-17 20:55:45 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,17 +3,24 @@
 
 using namespace std;
 
+// Zadanie. Napisz program, który odwróci otrzymany tekst od użytkownika.
+
 char wy(string wyraz)
 {
 	string odw;
-	for(int i=0;i<=wyraz.size();i++)odw[i]=wyraz[wyraz.size()-i];
-	for(int i=0;i<=wyraz.size();i++)cout<<odw[i];
+
+	for(int i=0;i<=wyraz.size();i++)
+	odw[i] = wyraz[wyraz.size()-i];
+	for(int i=0;i<=wyraz.size();i++)
+	cout << odw[i];
 }
 
 main()
 {
-	cout<<"Podaj wyraz ";
 	string wyraz;
+
+	cout<<"Podaj wyraz:\n";
 	cin>>wyraz;
+
 	wy(wyraz);
 }
